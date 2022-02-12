7ce117913bc6092c47510db1c6e134fdddaf98e2
Farhan070git <batch17070@gmail.com>
2022-02-13 02:13:05 +0500

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppComponent } from './app.component';
 import { MdbModule } from 'mdb-angular-ui-kit';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppointmentComponent } from './appointment/appointment.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CarouselComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    AppointmentComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
